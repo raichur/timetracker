cabc5bf6cb4fcd6e8ba98bda4547c7a211854198
Joshua Raichur <hello@joshuaraichur.com>
2014-12-31 08:59:10 +0530

    Add recentdays route

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,6 +1,10 @@
 class WorksController < ApplicationController
   def index
-    @works = Work.all
+    if (params[:days])
+      @works = Work.recentdays(params[:days]).order('datetimeperformed desc')
+    else
+      @works = Work.all.order('datetimeperformed desc')
+    end
   end
 
   def show
--- app/models/work.rb
+++ app/models/work.rb
@@ -9,7 +9,12 @@ class Work < ActiveRecord::Base
   validates :hours, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 8 }
 
   scope :fullday, -> { where("hours >= 8") }
-  scope :recent, -> { where("datetimeperformed > '#{Time.now - 7.days}'") }
+  # scope :recent, -> { where("datetimeperformed > '#{Time.now - 7.days}'") }
+
+  def self.recentdays(num_days_ago)
+    since_date = Time.now - num_days_ago.to_i.days
+    where("datetimeperformed > '#{since_date}'")
+  end
 
   def date_is_in_past
     if datetimeperformed.present? && datetimeperformed > Time.now

c2e2ebc9cebb5d35f65619187ede85140173a046
Joshua Raichur <hello@joshuaraichur.com>
2014-12-31 07:41:24 +0530

    Scopes

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,6 @@ class Project < ActiveRecord::Base
   belongs_to :company
   has_many :works
   has_many :users, :through => :works
+
+  scope :lowdefaultrate, -> { where("default_rate < 100") }
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,6 @@
 class Work < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
+  scope :fullday, -> { where("hours >= 8") }
+  scope :recent, -> { where("datetimeperformed > '#{Time.now - 7.days}'") }
 end

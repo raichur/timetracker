618f41f7f84c25343245de16ff7f48e4120132a0
Joshua Raichur <hello@joshuaraichur.com>
2014-12-31 08:15:14 +0530

    Finish validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,9 @@ class Project < ActiveRecord::Base
   has_many :works
   has_many :users, :through => :works
 
-  validates :name, presence: true
+  validates :name, length: { minimum: 5 }
+  validates :company, presence: true
+  validates :default_rate, numericality: { only_integer: true, greater_than: 50, less_than: 10000 }
 
   scope :lowdefaultrate, -> { where("default_rate < 100") }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   has_many :works
   has_many :projects, :through => :works
 
+  validates :fname, length: { minimum: 2 }
+  validates :lname, length: { minimum: 5 }
+  validates :company, presence: true
+
 end

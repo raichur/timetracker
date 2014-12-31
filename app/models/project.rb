87e69d740812a494846120f19af7146f161423b7
Joshua Raichur <hello@joshuaraichur.com>
2014-12-31 07:58:16 +0530

    Validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,5 +3,7 @@ class Project < ActiveRecord::Base
   has_many :works
   has_many :users, :through => :works
 
+  validates :name, presence: true
+
   scope :lowdefaultrate, -> { where("default_rate < 100") }
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,6 +1,9 @@
 class Work < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
+
+  validates :hours, numericality: { only_integer: true, greater_than: 0 }
+
   scope :fullday, -> { where("hours >= 8") }
   scope :recent, -> { where("datetimeperformed > '#{Time.now - 7.days}'") }
 end

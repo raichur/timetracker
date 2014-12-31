f4957d1c7e2b27c50d696ce7b0afe05346e8b0ea
Joshua Raichur <hello@joshuaraichur.com>
2014-12-31 14:34:22 +0530

    moar styling

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -5,5 +5,10 @@ class CompaniesController < ApplicationController
 
   def show
     @company = Company.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.xml { render xml: @company }
+      format.json { render json: @company }
+    end
   end
 end
